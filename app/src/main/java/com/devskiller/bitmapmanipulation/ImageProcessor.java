815fd7b934d5f5436cd11e8ca4384f39ba9a3524
Samuel <samuel.karanja@andela.com>
2022-06-23 13:47:31 +0300

    solution

--- app/src/main/java/com/devskiller/bitmapmanipulation/ImageProcessor.java
+++ app/src/main/java/com/devskiller/bitmapmanipulation/ImageProcessor.java
@@ -1,18 +1,27 @@
 package com.devskiller.bitmapmanipulation;
 
 import android.content.ContentResolver;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
 import android.location.Location;
 import android.net.Uri;
+import android.util.Log;
 
 import com.arasthel.asyncjob.AsyncJob;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import androidx.annotation.NonNull;
+import androidx.exifinterface.media.ExifInterface;
 
 final class ImageProcessor {
 
@@ -54,9 +63,71 @@ final class ImageProcessor {
                 final String outputPath = (outputDirectoryPath + fileName);
 
                 //START CHANGES
+                try{
+                    BitmapFactory.Options options = new BitmapFactory.Options();
+                    options.inJustDecodeBounds = true;
+                    InputStream input = mContentResolver.openInputStream(streamSource);
+                    BitmapFactory.decodeStream(input, null, options);
+
+                    // Calculate inSampleSize
+                    options.inSampleSize = calculateInSampleSize(options,mMaxDimension);
+
+                    // Decode bitmap with inSampleSize set
+                    options.inJustDecodeBounds = false;
+                    InputStream input2 = mContentResolver.openInputStream(streamSource);
+                    Bitmap bitmap = BitmapFactory.decodeStream(input2, null, options);
+                    Bitmap inverted = invert(bitmap);
+                    FileOutputStream outputStream = new FileOutputStream(outputPath);
+                    inverted.compress(Bitmap.CompressFormat.JPEG,COMPRESS_QUALITY_HINT,outputStream);
+                    outputStream.close();
+                    addExtraData(outputPath,note, location);
+                    callCallbackOnMainThread(new AsyncJob.OnMainThreadJob() {
+                        @Override
+                        public void doInUIThread() {
+                            callback.onSuccess(outputPath);
+                        }
+                    });
+                }catch (Exception exception){
+                    Log.e("ImageProcessor", "doOnBackground: "+exception.getMessage() );
+                    callCallbackOnMainThread(new AsyncJob.OnMainThreadJob() {
+                        @Override
+                        public void doInUIThread() {
+                            callback.onFailure();
+                        }
+                    });
+                }
                 //END CHANGES
             }
         }, mBackgroundExecutorService);
+    }
+
+    public Bitmap invert(Bitmap src)
+    {
+        int height = src.getHeight();
+        int width = src.getWidth();
+
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        Paint paint = new Paint();
+
+        ColorMatrix matrixGrayscale = new ColorMatrix();
+        matrixGrayscale.setSaturation(0);
+
+        ColorMatrix matrixInvert = new ColorMatrix();
+        matrixInvert.set(new float[]
+                {
+                        -1.0f, 0.0f, 0.0f, 0.0f, 255.0f,
+                        0.0f, -1.0f, 0.0f, 0.0f, 255.0f,
+                        0.0f, 0.0f, -1.0f, 0.0f, 255.0f,
+                        0.0f, 0.0f, 0.0f, 1.0f, 0.0f
+                });
+        matrixInvert.preConcat(matrixGrayscale);
+
+        ColorMatrixColorFilter filter = new ColorMatrixColorFilter(matrixInvert);
+        paint.setColorFilter(filter);
+
+        canvas.drawBitmap(src, 0, 0, paint);
+        return bitmap;
     }
 
     private int calculateInSampleSize(
@@ -64,7 +135,25 @@ final class ImageProcessor {
         int maxDimension
     ) {
         //START CHANGES
-        return 0;
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > maxDimension || width > maxDimension) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= maxDimension
+                    && (halfWidth / inSampleSize) >= maxDimension) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
         //END CHANGES
     }
 
@@ -74,6 +163,10 @@ final class ImageProcessor {
         final @NonNull Location location
     ) throws IOException {
         //START CHANGES
+        ExifInterface exif = new ExifInterface(bitmapPath);
+        exif.setAttribute(ExifInterface.TAG_IMAGE_DESCRIPTION,note);
+        exif.setGpsInfo(location);
+        exif.saveAttributes();
         //END CHANGES
     }
 
